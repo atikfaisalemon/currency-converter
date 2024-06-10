6defd193a90afaf00361dd897c08574fffe431f6
atikfaisalemon <atikfaisalemon@gmail.com>
2024-06-11 01:39:58 +0600

    working on input component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {} from "react";
+import "./App.css";
+import { Input } from "./components/Input";
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <Input />
+    </>
+  );
+}
+
+export default App;
